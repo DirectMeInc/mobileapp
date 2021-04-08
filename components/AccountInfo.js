c6ca8939befc07727659c2b0b00b56e28fb84802
Nicole Kim <na_yeon_kim@brown.edu>
2021-04-07 22:28:13 -0700

    transaction ui fix

--- components/AccountInfo.js
+++ components/AccountInfo.js
@@ -1,5 +1,5 @@
 import React, { Component, useContext } from 'react';
-import {StyleSheet, Alert, FlatList} from 'react-native';
+import {StyleSheet, Alert, FlatList, ScrollView} from 'react-native';
 import { View, Image, Text, Button, Colors, PanningProvider, Dialog, Constants, Slider, Card, ListItem } from 'react-native-ui-lib';
 import DropDownPicker from 'react-native-dropdown-picker';
 import ModalDropdown from 'react-native-modal-dropdown';
@@ -14,9 +14,51 @@ export default class AccountInfo extends React.Component {
             state: null,
         };
     }
+
+    renderRow(row, id) {
+    
+        return (
+          <View>
+            <ListItem
+              activeOpacity={0.3}
+              height={77.5}
+            >
+              <ListItem.Part left>
+                <Image
+                  source={{uri: row.mediaUrl}}
+                  style={styles.image}
+                />
+              </ListItem.Part>
+              <ListItem.Part middle column containerStyle={[styles.border, {paddingRight: 17}]}>
+                <ListItem.Part containerStyle={{marginBottom: 3}}>
+                  <Text style={{flex: 1, marginLeft: 10, fontWeight: 'bold', color: 'darkblue'}} numberOfLines={1}>{row.name}</Text>
+                  <Text style={{marginTop: 2, fontSize: 12, color: 'red'}}>{row.amount}</Text>
+                </ListItem.Part>
+                <ListItem.Part>
+                  <Text style={{flex: 1, marginLeft: 10}} text90 dark40 numberOfLines={1}>{row.time}</Text>
+                </ListItem.Part>
+              </ListItem.Part>
+            </ListItem>
+          </View>
+        );
+      }
     render() {
+        let orders = [
+            {
+                name: 'Netflix',
+                amount: '-$100.00',
+                time: 'Just now',
+            },
+            {
+                name: 'Spotify',
+                amount: '-$30.00',
+                time: '3 hrs',
+            }
+        ]
         return (
             <View style={{ flex: 1, backgroundColor: '#25315C'}}>
+            <View style={{ flexDirection: 'column', marginBottom: 0, paddingBottom: 0 }}>
+            <ScrollView style={{ marginBottom: 80, paddingTop: 10 }}>
                 <Header></Header>
 
                 <View style={{flexDirection:'row', marginTop: 30, marginLeft: 20}}>
@@ -42,22 +84,34 @@ export default class AccountInfo extends React.Component {
                     <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>Current Balance: <Text style={{fontSize: 20, color: '#6F8CF6', fontWeight:'normal'}}>-$100.00</Text></Text>
                 </View>
 
-                <Text style={{fontSize: 25, fontWeight: 'bold', color: 'white', marginTop: 30, marginLeft:20}}>Transactions</Text>
+                <Text style={{fontSize: 25, fontWeight: 'bold', color: '#6F8CF6', marginTop: 30, marginLeft:20}}>Transactions</Text>
 
-                <ListItem
-                    activeOpacity={0.3}
-                    height={60.5}
-                    containerStyle={{marginTop: 10, backgroundColor: '#C4C4C4'}}
-                >
-                    <View style={{marginTop: 5}}>
-                        <ListItem.Part>
-                            <Text style={{marginLeft: 5}}>Posted Date: <Text style={{fontWeight: 'bold'}}>1/15/21</Text></Text>
-                            <Text style={{marginLeft: 90}}>Current Balance: <Text style={{fontWeight: 'bold'}}>-$100.00</Text></Text>
-                        </ListItem.Part>
+                <Text style={{fontSize: 15, fontWeight: 'bold', color: '#dcdcdc', marginTop: 30, marginLeft:20}}>Today</Text>
+
+{/*                 <View style={{flexDirection:'row', marginTop: 20, justifyContent: 'center', alignItems: 'center'}}>
+                    <ListItem
+                        activeOpacity={0.3}
+                        height={60}
+                        containerStyle={{backgroundColor: '#e6f3ff', borderRadius: 30, width: '80%'}}
+                    >
+                    <View>
+                        <View style={{marginTop: 5}}>
+                            <ListItem.Part>
+                                <Text style={{marginLeft: 20, color: '#004480', fontWeight: 'bold', fontSize: 15}}>Merchant</Text>
+                                <Text style={{marginLeft: 150, color: '#009900', fontWeight: 'bold', fontSize: 15}}>-$100.00</Text>
+                            </ListItem.Part>
+                        </View>
+                        <View style={{marginTop: 2}}>
+                            <ListItem.Part>
+                                <Text style={{marginLeft: 20, color: '#004480', fontWeight: 'bold', fontSize: 12}}>Just now</Text>
+                            </ListItem.Part>
+                        </View>
                     </View>
-                </ListItem>
 
-                <ListItem
+                    </ListItem>
+                </View> */}
+
+{/*                 <ListItem
                     activeOpacity={0.3}
                     height={120.5}
                     containerStyle={{backgroundColor: '#E5E5E5'}}
@@ -83,25 +137,6 @@ export default class AccountInfo extends React.Component {
                                         state: item.value
                                     })}
                                 />
-{/*                                 <ModalDropdown 
-                                    options={['Other', 'Other']}
-                                    defaultValue='Select'
-                                    style={{ alignSelf: 'flex-end',
-                                    width: 55,
-                                    height: 35,
-                                    marginLeft: 10,
-                                    borderWidth: 0,
-                                    borderRadius: 10,
-                                    backgroundColor: 'cornflowerblue',}}
-                                    textStyle={{ marginVertical: 10,
-                                        marginHorizontal: 6,
-                                        fontSize: 12,
-                                        color: 'white',
-                                        fontWeight: 'bold',
-                                        textAlign: 'center',
-                                        textAlignVertical: 'center'}}
-                                    dropdownTextStyle={{textAlign: 'center'}}
-                                /> */}
                             </ListItem.Part>
                         </View>
 
@@ -113,10 +148,30 @@ export default class AccountInfo extends React.Component {
                         </View>
                     </View>
 
-                </ListItem>
+                </ListItem> */}
 
-                <ActionBarHome></ActionBarHome>
+            <View style={{marginTop: 10}}>
+                  <FlatList
+                        data={orders}
+                        renderItem={({item, index}) => this.renderRow(item, index)}
+                  />
+            </View>
 
+            <Text style={{fontSize: 15, fontWeight: 'bold', color: '#dcdcdc', marginTop: 30, marginLeft:20}}>30 April 2021</Text>
+
+            <View style={{marginTop: 10}}>
+                  <FlatList
+                        data={orders}
+                        renderItem={({item, index}) => this.renderRow(item, index)}
+                  />
+            </View>
+
+
+
+
+            </ScrollView>
+            </View>
+            <ActionBarHome></ActionBarHome>
             </View>
         )
     }
